b56e51cd184a81fda4ea0a075b11ccd82294e4eb
liz hdzj <lizbethhdz019@gmail.com>
2026-05-13 20:53:49 -0500

    Hice los cuadros de como seran los ctalogos al mostar el producto

--- Starducks/Vista/CatalogoPrincipal.Designer.cs
+++ Starducks/Vista/CatalogoPrincipal.Designer.cs
@@ -28,42 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            listBox1 = new ListBox();
-            dateTimePicker1 = new DateTimePicker();
-            domainUpDown1 = new DomainUpDown();
             SuspendLayout();
-            // 
-            // listBox1
-            // 
-            listBox1.FormattingEnabled = true;
-            listBox1.Location = new Point(237, 146);
-            listBox1.Name = "listBox1";
-            listBox1.Size = new Size(344, 244);
-            listBox1.TabIndex = 0;
-            // 
-            // dateTimePicker1
-            // 
-            dateTimePicker1.Location = new Point(257, 114);
-            dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(250, 27);
-            dateTimePicker1.TabIndex = 1;
-            // 
-            // domainUpDown1
-            // 
-            domainUpDown1.Location = new Point(407, 128);
-            domainUpDown1.Name = "domainUpDown1";
-            domainUpDown1.Size = new Size(150, 27);
-            domainUpDown1.TabIndex = 2;
-            domainUpDown1.Text = "domainUpDown1";
             // 
             // CatalogoPrincipal
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(domainUpDown1);
-            Controls.Add(dateTimePicker1);
-            Controls.Add(listBox1);
+            ClientSize = new Size(913, 697);
             Name = "CatalogoPrincipal";
             Text = "CatalogoPrincipal";
             Load += CatalogoPrincipal_Load;
@@ -71,9 +42,5 @@
         }
 
         #endregion
-
-        private ListBox listBox1;
-        private DateTimePicker dateTimePicker1;
-        private DomainUpDown domainUpDown1;
     }
 }
