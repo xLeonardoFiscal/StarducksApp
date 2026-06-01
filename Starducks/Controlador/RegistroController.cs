3e40cf4e5413385cd03fe6fac7498e31c4b2260e
Belinda <Belinda@LAPTOP-DMOKSG8S>
2026-06-01 12:01:45 -0500

    Actualizacion de registro

--- Starducks/Controlador/RegistroController.cs
+++ Starducks/Controlador/RegistroController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Starducks.Modelo;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,6 +10,28 @@ namespace Starducks.Controlador
         internal bool RegistroUsuario(string text1, string text2, string text3, string text4, string text5, string rutaFoto)
         {
             throw new NotImplementedException();
+        }
+
+        public bool RegistrarUsuario(
+             string nombre,
+             string usuario,
+             string password,
+             string telefono,
+             string direccion,
+             string foto)
+        {
+            Usuario nuevoUsuario = new Usuario();
+
+            nuevoUsuario.Nombre = nombre;
+            nuevoUsuario.User = usuario;
+            nuevoUsuario.Password = password;
+            nuevoUsuario.Telefono = telefono;
+            nuevoUsuario.Direccion = direccion;
+            nuevoUsuario.Foto = foto;
+
+            UsuarioDAO dao = new UsuarioDAO();
+
+            return dao.Insertar(nuevoUsuario);
         }
     }
 }
--- Starducks/Modelo/UsuarioDAO.cs
+++ Starducks/Modelo/UsuarioDAO.cs
@@ -1,10 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MySql.Data.MySqlClient;
 
 namespace Starducks.Modelo
 {
     internal class UsuarioDAO
     {
+        public bool Insertar(Usuario usuario)
+        {
+            try
+            {
+                MySqlConnection con = ConexionDB.ObtenerConexion();
+
+                string sql = @"INSERT INTO usuarios
+                               (Nombre, Usuario, Password, Telefono, Direccion, Foto)
+                               VALUES
+                               (@Nombre,@User,@Password,@Telefono,@Direccion,@Foto)";
+
+                MySqlCommand cmd = new MySqlCommand(sql, con);
+
+                cmd.Parameters.AddWithValue("@Nombre", usuario.Nombre);
+                cmd.Parameters.AddWithValue("@Usuario", usuario.User);
+                cmd.Parameters.AddWithValue("@Password", usuario.Password);
+                cmd.Parameters.AddWithValue("@Telefono", usuario.Telefono);
+                cmd.Parameters.AddWithValue("@Direccion", usuario.Direccion);
+                cmd.Parameters.AddWithValue("@Foto", usuario.Foto);
+
+                //Guarda la ruta de la imagen temporalmente
+                cmd.Parameters.AddWithValue("@foto", DBNull.Value);
+
+                int filas = cmd.ExecuteNonQuery();
+
+                con.Close();
+
+                return filas > 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al registrar usuario: " + ex.Message);
+                return false;
+            }
+        }
     }
 }
