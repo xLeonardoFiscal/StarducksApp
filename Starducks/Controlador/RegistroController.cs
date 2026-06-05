2ad32c782994dee2294f945d69c27e1c2934d0b9
Belinda <Belinda@LAPTOP-DMOKSG8S>
2026-06-04 22:37:06 -0500

    actualización del registro

--- Starducks/Controlador/RegistroController.cs
+++ Starducks/Controlador/RegistroController.cs
@@ -7,7 +7,13 @@ namespace Starducks.Controlador
 {
     public class RegistroController
     {
-        internal bool RegistroUsuario(string text1, string text2, string text3, string text4, string text5, string rutaFoto)
+        internal bool RegistroUsuario(
+            string text1,
+            string text2, 
+            string text3, 
+            string text4, 
+            string text5, 
+            string rutaFoto)
         {
             throw new NotImplementedException();
         }
